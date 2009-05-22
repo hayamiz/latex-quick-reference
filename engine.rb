abf20e19070156a40c812eb546b66e9794a72a36
Yuto Hayamizu <info@hayamin.com>
2009-05-22 14:50:02 +0900

    engine.rb supports smarks

--- engine.rb
+++ engine.rb
@@ -3,6 +3,8 @@
 $KCODE='u'
 
 ENV['GEM_PATH'] = "/home/haya/.gem/ruby/1.8"
+ENV['LQR_ROOT'] = File.dirname(__FILE__)
+$LQR_ROOT = ENV['LQR_ROOT']
 
 require 'cgi'
 require 'rubygems'
@@ -94,50 +96,75 @@ class SearchAction < ResponseAction
 
   # return HTML response or nil
   def filter_entry(entry_file)
-    doc = get_doc(entry_file)
-    return nil unless doc
+    docs = get_docs(entry_file)
+    return nil unless docs
     
-    return nil unless @query.match(doc.content)
-    doc.search(".subentry").each do |subentry|
-      subentry.remove() unless @subquery.match(subentry.content)
-    end
-    doc.search("li").each do |subentry|
-      subentry.remove() unless @subquery.match(subentry.content)
+    docs = docs.select do |doc|
+      if @query.match(doc.content)
+        doc.search(".subentry").each do |subentry|
+          subentry.remove() unless @subquery.match(subentry.content)
+        end
+        doc.search("li").each do |subentry|
+          subentry.remove() unless @subquery.match(subentry.content)
+        end
+      else
+        nil
+      end
     end
 
-    doc.to_html
+    docs.map{|doc| doc.to_html}.join("\n")
   end
 
   def entry_files()
     dir = File.dirname(__FILE__)
-    Dir.glob("#{dir}/entries/*.html") + Dir.glob("#{dir}/entries/*.markdown")
+    [Dir.glob("#{dir}/entries/*.html"),
+     Dir.glob("#{dir}/entries/*.markdown"),
+     Dir.glob("#{dir}/entries/*.scm")].flatten
   end
 
-  def get_doc(entry_file)
-    doc =
+  def get_docs(entry_file)
+    filedoc =
       case File.extname(entry_file)
-      when /html?/
+      when /html?/ # Raw html
         Nokogiri::HTML.fragment(open(entry_file).read)
-      when /markdown/
+      when /markdown/ # Markdown
         html = BlueCloth.new(open(entry_file).read).to_html
+        Nokogiri::HTML.fragment(html)
+      when /scm/ # smarks
+        html = `#{$LQR_ROOT}/gauche/src/gosh #{$LQR_ROOT}/lib/smarks.scm #{entry_file}`
         Nokogiri::HTML.fragment(html)
       else
         nil
       end
-    return nil unless doc
+    return nil unless filedoc
 
-    if doc.children.length > 1
-      html = doc.children.map{|c| c.to_html}.join("\n")
-      doc = Nokogiri::HTML.fragment("<div class=\"entry\">#{html}</div>")
+    ret = []
+    if filedoc.children.length > 1
+      entries = filedoc.xpath("div[@class='entry']")
+      if entries.empty?
+        html = filedoc.children.map{|c| c.to_html}.join("\n")
+        ret = [Nokogiri::HTML.fragment("<div class=\"entry\">#{html}</div>").children.first]
+      else
+        entries.each do |entry|
+          ret << entry
+        end
+      end
     else
-      root = doc.children.first
+      root = filedoc.children.first
       if root.name != "div" ||
           !root.attributes["class"] ||
           !root.attributes["class"].to_s.include?("entry")
-        doc = Nokogiri::HTML.fragment("<div class=\"entry\">#{root.to_html}</div>")
+        ret = [Nokogiri::HTML.fragment("<div class=\"entry\">#{root.to_html}</div>").children.first]
+      else
+        ret = [root]
       end
     end
-    doc = doc.children.first
+    
+    if ret.empty?
+      nil
+    else
+      ret
+    end
   end
 end
 
