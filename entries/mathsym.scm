122abd3d6cdd498980a22a79334744b2fbe11b7d
Yuto Hayamizu <info@hayamin.com>
2009-05-22 14:50:32 +0900

    updated mathsym.scm(smarks)

--- entries/mathsym.scm
+++ entries/mathsym.scm
@@ -1,5 +1,37 @@
 ;; -*- coding: utf-8 -*-
 
 (smarks-mathsymbol
- :keyword "math, symbol, operator"
- :symbols '(le leq ge geq times))
+ :title "Relational operator"
+ :keyword '("math" "symbol" "relational" "operator")
+ :symbols '(le leq prec preceq ll subset subseteq sqsubseteq vdash
+	       smile in notin ge geq succ succeq gg supset supseteq
+	       frown sqsupseteq dashv ni equiv sim simeq asymp
+	       approx cong neq doteq propto models perp mid parallel
+	       bowtie))
+
+(smarks-mathsymbol
+ :title "Binary operator"
+ :keyword '("math" "symbol" "binary" "operator")
+ :symbols '(pm mp times div ast star circ bullet cdot cap cup uplus
+	       sqcap sqcup vee wedge setminus wr diamond bigtriangleup
+	       bigtriangledown triangleleft triangleright oplus ominus
+	       otimes oslash odot bigcirc amalg ))
+
+(smarks-mathsymbol
+ :title "Relational operator(AMS Math)"
+ :keyword '("ams" "math" "symbol" "binary" "operator")
+ :symbols '(backsim
+	    backsimeq approxeq eqsim because between blacktriangleleft
+	    circeq eqcirc blacktriangleright bumpeq Bumpeq curlyeqprec
+	    curlyeqsucc preccurlyeq eqslantless eqslantgtr succcurlyeq
+	    leqq geqq gtrsim shortmid leqslant geqslant gtreqless
+	    gtreqqless gtrapprox lesseqgtr lesseqqgtr lessgtr lll
+	    llless ggg gggtr gtrless lesssim lessapprox multimap
+	    precsim succsim pitchfork doteqdot fallingdotseq
+	    risingdotseq shortparallel smallsmile smallfrown Subset
+	    Supset thicksim subseteqq supseteqq backepsilon
+	    vartriangleleft succapprox thickapprox vartriangleright
+	    precapprox triangleq trianglelefteq trianglerighteq 
+	    vartriangle Vdash Vvdash vDash varpropto ))
+
+
